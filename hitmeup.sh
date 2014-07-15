ac92204f1bdef2800b54a42c59af011967af3a60
mephizzle <mephistolomaniac@gmail.com>
2014-07-15 18:42:19 +0200

    initial commit

---
+++ hitmeup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+sudo apt-get update
+sudo apt-get -y install `cat apt_packages |tr "\n" " "`
+
+mkdir ~/src
+cd ~/src
+
+git clone https://github.com/mephizzle/dotfiles.git
+git clone https://github.com/mephizzle/bash_prompt.git
+
+ln -s ~/src/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/src/bash_prompt/bash_prompt ~/.bash_prompt
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# plugin-only vimrc
+ln -s ~/src/dotfiles/vim/vimrc_plugins ~/.vimrc
+
+vim +PluginInstall +qall
+
+rm ~/.vimrc
+ln -s ~/src/dotfiles/vim/vimrc_plugins ~/.vimrc_plugins
+ln -s ~/src/dotfiles/vim/vimrc ~/.vimrc
+
+echo ". ~/.bash_prompt" >> ~/.bashrc
