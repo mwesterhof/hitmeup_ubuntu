292365b430630f8f27888fc258e9b2a02a760848
Marco Westerhof <m.westerhof@lukkien.com>
2018-07-14 20:40:04 +0200

    Update hitmeup.sh

--- hitmeup.sh
+++ hitmeup.sh
@@ -9,7 +9,8 @@ sudo apt-get update
 sudo apt-get -y install `cat apt_packages |tr "\n" " "`
 sudo pip install -r pip_packages
 
-mkdir ~/src
+mkdir -p ~/src
+mkdir -p ~/dev
 cd ~/src
 
 git clone https://github.com/mwesterhof/dotfiles.git
@@ -20,19 +21,17 @@ chsh -s `which zsh`
 ln -s ~/src/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/src/bash_prompt/bash_prompt ~/.bash_prompt
 
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # plugin-only vimrc
-ln -s ~/src/dotfiles/vim/vimrc_plugins ~/.vimrc
-
+ln -s ~/src/dotfiles/vim/vimrc_plugins ~/.config/nvim/init.vim
 vim +PlugInstall +qall
-
-rm ~/.vimrc
+rm ~/.config/nvim/init.vim
 ln -s ~/src/dotfiles/vim/vimrc_plugins ~/.vimrc_plugins
-ln -s ~/src/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/src/dotfiles/vim/vimrc ~/.config/nvim/init.vim
 
-ln -s  ~/src/dotfiles/ack/ackrc ~/.ackrc
-ln -s  ~/src/dotfiles/gitconfig ~/.gitconfig
-ls -s  ~/src/dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s  ~/src/dotfiles/zshell/zshrc ~/.zshrc
+ln -s ~/src/dotfiles/ack/ackrc ~/.ackrc
+ln -s ~/src/dotfiles/gitconfig ~/.gitconfig
+ls -s ~/src/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/src/dotfiles/zshell/zshrc ~/.zshrc
+ln -s ~/src/dotfiles/xonsh/xonsh.rc ~/.xonshrc
